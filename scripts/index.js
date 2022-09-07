9397abdaf4dc566cec7b6b5b0384a0314f87381b
shimapi <shirley.mallea2@gmail.com>
2022-09-07 12:34:15 -0400

    fix: before JS

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let editName = document.querySelector(".owner__info_edit");
+
+function editOwner(){
+  console.log('haz hecho click en editar');
+}
+
+editName.addEventListener('click', editOwner);
