d6dbb8421e989918288fa59969c29fe7269a9a7d
shimapi <shirley.mallea2@gmail.com>
2022-10-20 20:13:52 -0300

    refactor: popup edit open and close editProfile & addPlace

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-const openEditButton = document.querySelector(".button__edit");
-const openAddButton = document.querySelector(".button__add");
+const openEditProfileButton = document.querySelector(".button__edit");
+const openAddPlaceButton = document.querySelector(".button__add");
+
 const closeModalButtons = document.querySelectorAll(".button__close");
 const modals = document.querySelectorAll(".modal");
 
@@ -7,6 +8,20 @@ const originalName = 'Jacques Cousteau';
 const originalAbout = 'Explorer';
 
 const openEditProfilePopUp = document.querySelector(".edit-profile");
+const openAddPlacePopUp = document.querySelector(".add-place");
+const openPhotoPopUp = document.querySelector(".photo-popup");
+
+const buttons = document.querySelectorAll("[data-target]");
+
+buttons.forEach((btn)=>{
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.remove("modal__inactive");
+    document.querySelector(btn.dataset.target).classList.add("modal__active");
+    console.log(btn)
+  })
+})
+
+
 
 function infoInicial(){
   document.querySelector(".main-text__name").textContent = originalName;
@@ -15,34 +30,62 @@ function infoInicial(){
   document.querySelector(".edit-profile__about").value = originalAbout;
 }
 
-//en esta funcion estan apareciendo los tres modales: editar perfil, añadir foto y photo popup
+/*function handleOpenPopup(){
+  openEditProfilePopUp.classList.remove("modal__inactive");
+  openEditProfilePopUp.classList.add("modal__active");
+  //console.log(openEditProfilePopUp) -> funciona :D
+}*/
+
+function handleOpenPopup(event){
+  modals.forEach((modal) => {
+    
+    modal.addEventListener("click", (e)=>{
+      console.log(e.target.classList.contains("edit-profile__container"))
+      console.log(e.target)
+
+    })
+    modal.classList.remove("modal__inactive");
+    modal.classList.add("modal__active");
+    console.log(modal) //OK. -> 3 popups
+  })
+}
+
+//en esta funcion estan apareciendo los tres modales: editar perfil, añadir foto y photo popup -> debería mejorarse.
 function handleCloseModal(event){
   modals.forEach((modal) => {
     modal.classList.remove("modal__active");
     modal.classList.add("modal__inactive");
-    console.log(modal)
+    //console.log(modal) OK. -> 3 popups
   })
 }
 
-function handleOpenPopup(){
-  openEditProfilePopUp.classList.remove("modal__inactive");
-  openEditProfilePopUp.classList.add("modal__active");
-
-  console.log(openEditProfilePopUp)
-}
-
-
-
-function handleEditProfilePopUp(){
-  handleOpenPopup()
-  //editProfile.addEventListener("click",handleCloseModalOut)
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+ // handleOpenPopup();
   const newName = document.querySelector(".edit-profile__name").value;
   const newAbout = document.querySelector(".edit-profile__about").value;
   document.querySelector(".main-text__name").textContent = newName;
   document.querySelector(".main-text__about").textContent = newAbout;
 }
 
-openEditButton.addEventListener('click', handleEditProfilePopUp);
+
+function handleAddPlace() {
+ // handleOpenPopup()
+  const newPhoto = document.querySelector(".add-place__name").value;
+  const newLink = document.querySelector(".add-place__link").value;
+  console.log(newPhoto);
+  console.log(newLink);
+}
+
+
+
+openEditProfileButton.addEventListener('click', handleProfileFormSubmit);
+openEditProfilePopUp.addEventListener('submit', handleProfileFormSubmit);
+openEditProfilePopUp.addEventListener('submit', handleCloseModal); 
+
+openAddPlaceButton.addEventListener('click', handleAddPlace);
+openAddPlaceButton.addEventListener('submit', handleAddPlace);
+openAddPlaceButton.addEventListener('submit', handleCloseModal); 
 
 closeModalButtons.forEach((button) => {
   button.addEventListener("click",handleCloseModal);
