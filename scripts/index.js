924bbbc2708fe81d2d67a4a7eae37865fb0a701d
shimapi <shirley.mallea2@gmail.com>
2022-10-20 18:38:36 -0300

    refactor: popup edit open and close

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 const openEditButton = document.querySelector(".button__edit");
 const openAddButton = document.querySelector(".button__add");
 const closeModalButtons = document.querySelectorAll(".button__close");
+const modals = document.querySelectorAll(".modal");
 
 const originalName = 'Jacques Cousteau';
 const originalAbout = 'Explorer';
+
+const openEditProfilePopUp = document.querySelector(".edit-profile");
 
 function infoInicial(){
   document.querySelector(".main-text__name").textContent = originalName;
@@ -12,15 +15,37 @@ function infoInicial(){
   document.querySelector(".edit-profile__about").value = originalAbout;
 }
 
-function createCard(){}
+//en esta funcion estan apareciendo los tres modales: editar perfil, añadir foto y photo popup
+function handleCloseModal(event){
+  modals.forEach((modal) => {
+    modal.classList.remove("modal__active");
+    modal.classList.add("modal__inactive");
+    console.log(modal)
+  })
+}
 
-function closePopUp(){}
+function handleOpenPopup(){
+  openEditProfilePopUp.classList.remove("modal__inactive");
+  openEditProfilePopUp.classList.add("modal__active");
 
-function openPopUp(){}
+  console.log(openEditProfilePopUp)
+}
 
-function validateForm(){}
 
-function showImageInPopup(){}
 
+function handleEditProfilePopUp(){
+  handleOpenPopup()
+  //editProfile.addEventListener("click",handleCloseModalOut)
+  const newName = document.querySelector(".edit-profile__name").value;
+  const newAbout = document.querySelector(".edit-profile__about").value;
+  document.querySelector(".main-text__name").textContent = newName;
+  document.querySelector(".main-text__about").textContent = newAbout;
+}
+
+openEditButton.addEventListener('click', handleEditProfilePopUp);
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click",handleCloseModal);
+})
 
 infoInicial();
