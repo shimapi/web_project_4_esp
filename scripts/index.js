bed54fbf14857c44d7b26041bef9a76169bda45a
shimapi <shirley.mallea2@gmail.com>
2022-10-29 14:29:01 -0300

    fix: trying to fix modal opening

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,3 @@
-/* 
-Nota al revisor: hice los cambios en el modal, sin embargo, no consigo que se vea.
-Por eso había incluido las clases auxiliares inactive y closing.
-Agradecería alguna pista para solucionarlo.
-
-Template listo :)
-*/
-
 const originalName = "Jacques Cousteau";
 const originalAbout = "Explorer";
 
