23c63e80e8eb6965a75434e2f03b5762c93daed6
shimapi <shirley.mallea2@gmail.com>
2023-03-31 14:46:04 -0300

    feat: moving project to webpack structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-const numbers = [2, 3, 5];
+// index.js
 
-// Función arrow. ¿Cómo se las arreglará Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2); 
+import "./styles/index.css"; // agrega la importación del archivo principal de hojas de estilo
+//import stepsSrc from "./images/steps.png";
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
 
 console.log(doubledNumbers); // 4, 6, 10
+
+//const stepsImage = document.getElementById("image-steps");
+//stepsImage.src = stepsSrc;
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,56 @@
 // webpack.config.js
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
   devtool: 'inline-source-map',
   entry: {
-    main: './src/index.js'
+    main: "./src/index.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: ""
   },
-  target: ['web', 'es5'], // asegúrate de que el código glue de Webpack sea también compatible con ES5
-  mode: 'development',
+  target: ['web', 'es5'],
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true
   },
   module: {
-    rules: [ // esto es un array de reglas
-      // añádele un objeto que contenga reglas para Babel
+    rules: [
       {
-        // una expresión regular que busca todos los archivos js
         test: /\.js$/,
-        // todos los archivos deben ser procesados por babel-loader
         loader: "babel-loader",
-        // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
         exclude: "/node_modules/"
-      }
+      },
+    /*  {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+        ],
+      },*/
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      { test: /\.txt$/, use: 'raw-loader' }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 }
