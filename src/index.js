15bf709248a8aad8b45cc1a5bc91bca5fc05674f
shimapi <shirley.mallea2@gmail.com>
2023-04-03 16:06:34 -0400

    feat: trying to fix webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
-// index.js
+import Section from "./scripts/Section.js";
+import Card from "./scripts/Card.js";
+import UserInfo from "./scripts/UserInfo.js";
+import PopupWithForm from "./scripts/PopupWithForm.js";
+import {
+  initialCards,
+  cardsContainer,
+  openAddPlacePopUp,
+  openEditProfilePopUp,
+  openEditProfileButton,
+  openAddPlaceButton,
+  config,
+  textName,
+  textAbout,
+  originalAbout,
+  originalName,
+} from "./scripts/constants.js";
 
-import "./styles/index.css"; // agrega la importación del archivo principal de hojas de estilo
-//import stepsSrc from "./images/steps.png";
+const handleInitialCards = new Section ({
+  data: initialCards,
+  renderer: (cardNewItem) => {
+    const createNewCard = new Card(cardNewItem, config.cardTemplate);
+    cardsContainer.appendChild(createNewCard.generateCard());     
+  }
+});
 
-const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map(number => number * 2);
+const handleProfileFormSubmit = () => {
+  const profileForm = document.forms.editProfile;
+  const newName = profileForm.elements.editProfileName;
+  const newAbout = profileForm.elements.editProfileAbout;
 
-console.log(doubledNumbers); // 4, 6, 10
+  AddUserInfo.setUserInfo(newName.value, newAbout.value);
+  PopUpEditProfile.close();
+}
 
-//const stepsImage = document.getElementById("image-steps");
-//stepsImage.src = stepsSrc;
+const handleAddPlaceFormSubmit = () => {
+  const addPlaceForm = document.forms.addPlace;
+  const addPlaceName = addPlaceForm.elements.addPlaceName;
+  const addPlaceLink = addPlaceForm.elements.addPlaceLink;
+
+  const cardNewItem = JSON.parse(`{"name": "${addPlaceName.value}", "link": "${addPlaceLink.value}"}`);
+
+  const createNewCard = new Card(cardNewItem, config.cardTemplate);
+  cardsContainer.prepend(createNewCard.generateCard());
+  PopUpAddPhoto.close();
+}
+
+const PopUpEditProfile  = new PopupWithForm(openEditProfilePopUp);
+const PopUpAddPhoto     = new PopupWithForm(openAddPlacePopUp);
+const AddUserInfo       = new UserInfo(textName, textAbout);
+
+
+openEditProfileButton.addEventListener("click", () => {
+  AddUserInfo.getUserInfo();
+  PopUpEditProfile._getInputValues();
+  PopUpEditProfile.open();
+});
+
+openAddPlaceButton.addEventListener("click", () => {
+  PopUpAddPhoto.open();
+});
+
+openEditProfilePopUp.addEventListener("submit", handleProfileFormSubmit);
+openAddPlacePopUp.addEventListener("submit", handleAddPlaceFormSubmit);
+
+handleInitialCards.renderItems();
+
+AddUserInfo.setUserInfo(originalName,originalAbout);
--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,63 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   entry: {
-    main: "./src/index.js"
+    main: "./src/index.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: ""
+    publicPath: "",
   },
-  target: ['web', 'es5'],
+  target: ["web", "es5"],
+  stats: { children: true },
   mode: "development",
   devServer: {
     static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
-    open: true
+    open: true,
   },
   module: {
     rules: [
+      // esto es un array de reglas
+      // añádele un objeto que contenga reglas para Babel
       {
+        // una expresión regular que busca todos los archivos js
         test: /\.js$/,
+        // todos los archivos deben ser procesados por babel-loader
         loader: "babel-loader",
-        exclude: "/node_modules/"
+        // excluye la carpeta node_modules, no necesitamos procesar archivos en ella
+        exclude: "/node_modules/",
       },
-    /*  {
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader"
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
           },
+          "postcss-loader",
         ],
-      },*/
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-      { test: /\.txt$/, use: 'raw-loader' }
-    ]
+      {
+        // añade la regla para el procesamiento de archivos
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/index.html", // ruta a nuestro archivo index.html
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
-  ]
-}
+    new MiniCssExtractPlugin(), // conecta el plugin para fusionar archivos CSS
+  ],
+};
